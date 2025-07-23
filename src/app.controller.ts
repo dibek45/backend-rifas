174bdde0668f3578f15ab681083bfde2fd8aee25
Dibek <dibektube@gmail.com>
2025-07-22 21:02:13 -0600

    Regenerar Prisma con openssl 1.1.222x

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-    @Get('health')
+    @Get('health1')
   getHealth() {
     return { status: 'ok desde rifas' };
   }
