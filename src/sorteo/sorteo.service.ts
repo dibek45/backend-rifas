2d86424be00f40a50df6bd126bb71499605da1f9
Dibek <dibektube@gmail.com>
2025-07-23 12:12:18 -0600

    R12

--- src/sorteo/sorteo.service.ts
+++ src/sorteo/sorteo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Prisma } from '../../generated/prisma';
+import { Prisma } from '@prisma/client';
 import { UpdateSorteoDto } from './dto/update-sorteo.dto';
 
 @Injectable()
