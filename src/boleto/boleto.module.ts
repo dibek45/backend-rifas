99c3690e72def60f3736fd0161ca737a4e6a8144
Dibek <dibektube@gmail.com>
2025-07-23 13:05:20 -0600

    feat: implement boleto service, controller and DTO

--- src/boleto/boleto.module.ts
+++ src/boleto/boleto.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoletoService } from './boleto.service';
 import { BoletoController } from './boleto.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+    imports: [PrismaModule], // 👈 agrégalo aquí
+  
   controllers: [BoletoController],
   providers: [BoletoService],
 })
