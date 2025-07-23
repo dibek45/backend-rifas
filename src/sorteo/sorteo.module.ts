b1eb8083a449478ccaef406b6f90cd401a96330f
Dibek <dibektube@gmail.com>
2025-07-23 13:46:18 -0600

    vendedo2

--- src/sorteo/sorteo.module.ts
+++ src/sorteo/sorteo.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SorteoService } from './sorteo.service';
 import { SorteoController } from './sorteo.controller';
 import { PrismaModule } from '../prisma/prisma.module'; // 👈 importa el módulo de Prisma
+import { BoletoService } from 'src/boleto/boleto.service';
 
 @Module({
   imports: [PrismaModule], // 👈 agrégalo aquí
   controllers: [SorteoController],
-  providers: [SorteoService],
+  providers: [SorteoService,BoletoService],
 })
 export class SorteoModule {}
