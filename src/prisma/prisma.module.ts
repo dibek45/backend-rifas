0b6a5ab126a7f6bf2f9874f09378af9ad4285679
Dibek <dibektube@gmail.com>
2025-07-22 20:30:41 -0600

    🔥 Build lrifas03

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService] // 👈 ¡Esto permite usarlo en otros módulos!
 })
 export class PrismaModule {}
--- src/sorteo/sorteo.module.ts
+++ src/sorteo/sorteo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SorteoService } from './sorteo.service';
 import { SorteoController } from './sorteo.controller';
+import { PrismaModule } from '../prisma/prisma.module'; // 👈 importa el módulo de Prisma
 
 @Module({
+  imports: [PrismaModule], // 👈 agrégalo aquí
   controllers: [SorteoController],
   providers: [SorteoService],
 })
