3b3cca56383fc7be5e46b6dfc8505db60aef88b4
Dibek <dibektube@gmail.com>
2025-07-25 11:15:28 -0600

    rifas complet8

--- src/comprador/dto/create-comprador.dto.ts
+++ src/comprador/dto/create-comprador.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class CreateCompradorDto {
   @IsString()
@@ -6,4 +6,9 @@ export class CreateCompradorDto {
 
   @IsEmail()
   email: string;
+
+  @IsOptional()
+  @IsString()
+  telefono?: string;
 }
+
