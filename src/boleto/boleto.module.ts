b2476efc25a54760209c5a794263504cc7b563bc
Dibek <dibektube@gmail.com>
2025-08-02 13:56:52 -0600

    rpisma11

--- src/boleto/boleto.module.ts
+++ src/boleto/boleto.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // 👈 necesitas esto
 import { BoletoService } from './boleto.service';
 import { BoletoController } from './boleto.controller';
 import { SorteoGateway } from 'src/sockets/boletos.gateway';
 import { Boleto } from './entities/boleto.entity';
+import { Comprador } from '../comprador/entities/comprador.entity';
+import { Sorteo } from '../sorteo/entities/sorteo.entity';
 
 @Module({
-    imports: [Boleto], // 👈 agrégalo aquí
-    exports: [BoletoService], // 👈 esto es CLAVE
-
+  imports: [
+    TypeOrmModule.forFeature([Boleto, Comprador, Sorteo]), // ✅ CORRECTO
+  ],
   controllers: [BoletoController],
-  providers: [BoletoService,SorteoGateway],
+  providers: [BoletoService, SorteoGateway],
+  exports: [BoletoService],
 })
 export class BoletoModule {}
