d7c4554095402af5133b380862c5c02b1917e42c
Dibek <dibektube@gmail.com>
2025-08-02 20:38:06 -0600

    rpisma17

--- src/comprador/comprador.module.ts
+++ src/comprador/comprador.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompradorService } from './comprador.service';
 import { CompradorController } from './comprador.controller';
+import { Comprador } from './entities/comprador.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Comprador]), // 👈 esto es lo que faltaba
+  ],
   controllers: [CompradorController],
   providers: [CompradorService],
+  exports: [CompradorService], // opcional si lo usas en otros módulos
 })
 export class CompradorModule {}
