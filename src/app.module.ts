3870a3e838e1d077a11926b7bb13f6eba6ada543
Dibek <dibektube@gmail.com>
2025-08-18 15:00:38 -0600

    agenda1

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,13 @@ import { Vendedor } from './vendedor/entities/vendedor.entity';
 import { Comprador } from './comprador/entities/comprador.entity';
 import { Boleto } from './boleto/entities/boleto.entity';
 import { CuentaBancaria } from './vendedor/entities/cuenta-bancaria.entity';
+import { AdminModule } from './agenda/admin-agenda/admin.module';
+import { EventoModule } from './agenda/evento/evento.module';
+import { CitaModule } from './agenda/cita/cita.module';
+import { Cita } from './agenda/cita/cita.entity';
+import { Evento } from './agenda/evento/entities/evento.entity';
+import { Admin } from './agenda/admin-agenda/entities/admin.entity';
+
 
 @Module({
   imports: [
@@ -21,11 +28,17 @@ import { CuentaBancaria } from './vendedor/entities/cuenta-bancaria.entity';
     SorteoModule,
     CompradorModule,
     VendedorModule,
+      AdminModule,
+  EventoModule,
+  CitaModule,
    AuthModule,
     TypeOrmModule.forRoot({
       type: 'postgres',  // Asegúrate de usar PostgreSQL
       url: process.env.DATABASE_URL,  // Usamos la URL de conexión desde las variables de entorno
-      entities: [Usuario, Sorteo, Comprador, Vendedor, Boleto, CuentaBancaria],  // Agregamos todas las entidades
+      entities: [Usuario, Sorteo, Comprador, Vendedor, Boleto, CuentaBancaria,
+         Admin,
+      Evento,
+      Cita],  // Agregamos todas las entidades
       synchronize: false,  // Ten cuidado con esta opción en producción
       autoLoadEntities: false,  // 🔥 Deja que Nest cargue automáticamente tus entidades
 
